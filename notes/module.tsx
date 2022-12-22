124e5461810837c25f092999e29915b8d912b2d1
whatcodehaveyougit <siggywatt@yahoo.com>
2022-12-22 14:04:24 +0000

    added afew inputs and all is working, although i cannot fully see what is in the store.  also it turns out you really do not need local state.

---
+++ notes/module.tsx
@@ -0,0 +1,46 @@
+// ============== The Store
+
+// The store is the entire store of state for your application.
+
+// If everytime you wanted to update the state of the application everything got reloaded
+// again that would not be effecient.
+
+// For this reason we have Modules.  You attach state to each module and when a piece of state
+// in a module updates, it will just reload the components of that module, rather
+// than the entire application = more performant.
+
+// A store is instantiated like this:
+
+const myStore = new Store();
+
+// ============= Modules
+
+// After creating a store it is then adviseable to register modules to it, for example:
+
+myStore.register('exampleForm', exampleForm);
+
+// The frame work for a module is as follows
+
+const exampleForm: Module<{ name: string; }> = {
+  state: {
+    name: '',
+  },
+  mutations: {
+  },
+  actions: {
+  },
+};
+
+// Using Modules and the Store together
+
+// Now that we have setup our store and added a module to it we will now start calling
+// method on the store using that module.
+
+myStore.mutate('nameOfModule', 'MUTATION_NAME', { field: 'name', value: e.target.value });
+
+myStore.mutate('exampleForm', 'CHANGE_VALUE_BANANA', { field: 'name', value: e.target.value });
+
+// Here we are calling the `mutate` method on myStore, we specify the name of the Module and then
+// pass something through to the mutation.
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Store, { Module } from 'diox';
-const store = new Store();
+const myStore = new Store();
 
 
-const exampleForm: Module<{ name: string; }> = {
+const exampleForm: Module<{ firstName: string, lastName: string, age: number }> = {
   state: {
-    name: 'testing',
+    firstName: '',
+    lastName: '',
+    age: 0
   },
   mutations: {
-    CHANGE_VALUE({ state }, data) {
+    CHANGE_VALUE_BANANA({ state }, data) {
       return {
         ...state,
         [data.field]: data.value,
+      };
+    },
+    CHANGE_VALUE_SIMPLE_STATE({ state }, data) {
+      return {
+        ...state,
+        age: data.value,
       };
     },
   },
@@ -20,32 +28,45 @@ const exampleForm: Module<{ name: string; }> = {
   },
 };
 
-store.register('exampleForm', exampleForm);
+myStore.register('exampleForm', exampleForm);
 
-store.subscribe('exampleForm', (newState) => {
+myStore.subscribe('exampleForm', (newState) => {
   console.log('New mutation ! ', newState);
 });
 
 function App() {
 
-  const [name, setName] = useState<any>();
+  // const [name, setName] = useState<any>();
 
-  useEffect(() => {
-    store.subscribe('exampleForm', (newState) => {
-      setName(newState)
-    })
-  }, [])
+  // useEffect(() => {
+  //   store.subscribe('exampleForm', (newState) => {
+  //     setName(newState)
+  //   })
+  // }, [])
 
   return (
     <div className="App">
-      {/* <div>{state}</div> */}
+      <div>{JSON.stringify(myStore)}</div>
+      {/* <div>{myStore.modules.exampleForm.state.firstName}</div> */}
+
       <div>
-        <label>Name: </label>
-        <input onChange={(e) => {
-          store.mutate('exampleForm', 'CHANGE_VALUE', { field: 'name', value: e.target.value });
+        <label>First Name: </label>
+        <input type="text" onChange={(e) => {
+          myStore.mutate('exampleForm', 'CHANGE_VALUE_BANANA', { field: 'firstName', value: e.target.value });
         }} />
       </div>
-
+      <div>
+        <label>Last Name: </label>
+        <input type="text" onChange={(e) => {
+          myStore.mutate('exampleForm', 'CHANGE_VALUE_BANANA', { field: 'lastName', value: e.target.value });
+        }} />
+      </div>
+      <div>
+        <label>Age: </label>
+        <input type="number" onChange={(e) => {
+          myStore.mutate('exampleForm', 'CHANGE_VALUE_SIMPLE_STATE', { value: e.target.value });
+        }} />
+      </div>
     </div>
   );
 }
